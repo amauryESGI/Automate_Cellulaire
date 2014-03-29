aed6173798aa14a286877662b80533eca150dc97
amauryESGI <amaury.colmant@wanadoo.fr>
2014-03-29 12:00:48 +0100

    base
    Ajout de la structure de base au project.

---
+++ Assets/Scripts/UI/mainInterface.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainInterface : MonoBehaviour
+{
+    private string affichage;
+
+    private void Start() {
+        affichage = "3D";
+    }
+
+    private void OnGUI()
+    {
+        GUI.BeginGroup(new Rect(20, 20, Screen.width - 40, Screen.height - 40));
+        
+        if (GUI.Button(new Rect(0, 0, 35, 25), affichage)) {
+            if (affichage == "3D") {
+                affichage = "2D";
+            } else {
+                affichage = "3D";
+            }
+        }
+    }
+}
